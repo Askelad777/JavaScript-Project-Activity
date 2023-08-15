c42f9e3176db68924c6c2e5f8211ff2ae04e8e5b
Askelad777 <xypcollenescadeer@gmail.com>
2023-08-16 00:14:54 +0800

    commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,11 @@
+ // parse Int/Float/etc. Is a way to convert the text the way we wanted it to be. 
+  // browser  to JS is always on the text so this method is somehow the way how to make it work
+  // in a mathematical way.
+
+  //on the other hand if we want to convert a number into a string just simply add next to the variable
+  //variableName.toString()
+
+
 const defaultResult = 0;
 let currentResult = defaultResult;
 
@@ -6,13 +14,10 @@ let currentResult = defaultResult;
 function getUserNumInput(){
     return parseInt(userInput.value);
 }
-function add(){
-  // parse Int/Float/etc. Is a way to convert the text the way we wanted it to be. 
-  // browser  to JS is always on the text so this method is somehow the way how to make it work
-  // in a mathematical way.
 
-  //on the other hand if we want to convert a number into a string just simply add next to the variable
-  //variableName.toString()
+
+function add(){
+ 
   const enteredNumber = getUserNumInput();
   const calcDescription = `${currentResult} + ${enteredNumber}`
   currentResult = currentResult + enteredNumber;
@@ -20,4 +25,28 @@ function add(){
   
 }
 
+function subtract(){
+  const enteredNumber = getUserNumInput();
+  const calcDescription = `${currentResult} - ${enteredNumber}`
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function multiply(){
+  const enteredNumber = getUserNumInput();
+  const calcDescription = `${currentResult} * ${enteredNumber}`
+  currentResult = currentResult * enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
+function divide(){
+  const enteredNumber = getUserNumInput();
+  const calcDescription = `${currentResult} / ${enteredNumber}`
+  currentResult = currentResult / enteredNumber;
+  outputResult(currentResult, calcDescription);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
