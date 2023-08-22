3a2145ec05d0a040890ddf49c3a15b734491eac8
ESCADER, XYP COLLEN <116700665+Askelad777@users.noreply.github.com>
2023-08-22 09:13:45 +0800

    commit

--- functions-assignment-problem/assignment.js
+++ functions-assignment-problem/assignment.js
@@ -1,43 +1,42 @@
 
-// TASK #1
-const person1 = 'Xyp Collen Escader';
-const greeting = (name) => console.log(`Hello there! ${name}`);
+// // TASK #1
+// const person1 = 'Xyp Collen Escader';
+// const greeting = (name) => console.log(`Hello there! ${name}`);
 
 
 
-// TASK #2
+// // TASK #2
 
-// T2.A
-const messageTemplate = 'How are you today ?';
-const greetTemplate = (mTemplate,name) => console.log(mTemplate + name);
+// // T2.A
+// const messageTemplate = 'How are you today ?';
+// const greetTemplate = (mTemplate,name) => console.log(mTemplate + name);
 
-// T2.B
+// // T2.B
 
-const messageToUser = () => console.log('How may I help you Mr. Escader?');
+// const messageToUser = () => console.log('How may I help you Mr. Escader?');
 
-// T2.C 
+// // T2.C 
 
-const userInteraction = (name) => {
-    return `Let me know if you need anything ${name}`;
-};
+// const userInteraction = (name) => {
+//     return `Let me know if you need anything ${name}`;
+// };
 
 
-greeting(person1);
-greetTemplate(messageTemplate, person1);
-messageToUser();
-userInteraction(person1);
+// greeting(person1);
+// greetTemplate(messageTemplate, person1);
+// messageToUser();
+// userInteraction(person1);
 
 
 // TASK 3 
-const Utext = prompt('Please enter a message: ','');
-const Uname = prompt('Please enter a name: ','')
+// const Utext = prompt('Please enter a message: ','');
+// const Uname = prompt('Please enter a name: ','')
 const userMessage = (userName, userText = 'How are you to day?') =>{
   alert(`${userName} ${userText}`);
   console.log(`${userName} ${userText}`);
 };
 
-userMessage(Uname, Utext);
-
+userMessage('Xyp')
 // TASK 4
 
 const checkInput = (resultHandler,...valString) =>{
