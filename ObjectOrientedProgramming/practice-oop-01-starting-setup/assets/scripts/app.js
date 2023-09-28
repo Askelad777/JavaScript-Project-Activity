67e99384cdc783b017b32011da63dccbc4a3a954
ESCADER, XYP COLLEN <116700665+Askelad777@users.noreply.github.com>
2023-09-28 23:37:27 +0800

    commit

--- ObjectOrientedProgramming/practice-oop-01-starting-setup/assets/scripts/app.js
+++ ObjectOrientedProgramming/practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,36 +1,91 @@
-class Tooltip{}
+class ToolTip{}
+
+
+
+
 
 class ProjectItem{
-  constructor(id){
+  constructor(id, updateProjectListFunction){
     this.id = id;
+    this.updateProjectListHandler = updateProjectListFunction;
     this.connectMoreInfoButton();
-    this.connectSwitchButton();  
+    this.connectSwitchButton();
   }
-
 
   connectMoreInfoButton(){}
-  connectSwitchButton(){
-    const projectItemELement = document.getElementById(this.id);
-    const switchBtn = projectItemELement.querySelector('button:last-of-type');
 
-    switchBtn.addEventListener('click', )
+  connectSwitchButton(){
+
+    // Accessing the this.id 
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click', this.updateProjectListHandler)
   }
 }
+
+
+
+
+
 
 class ProjectList{
+
   projects = [];
+
   constructor(type){
-    const projectItems = document.querySelectorAll(`#${type}-projects li`);
-    for(const prjItem of projectItems){
-      this.projects.push(new ProjectItem(prjItem.id));      
+    this.type = type;
+    //  prjItems variable key accessing DOM elements using queryselectorAll.
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    // Fetch project item from the DOM to get information
+    for(const prjItem of prjItems){
+      // accessing the specific Id of the dom Node
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+        );
     }
+    console.log(this.projects);
+    
+  }
+
+  setSwitchHandlerFunction(switchHandlerFunction){
+    // pointer to the switchHandlerFunction. 
+     this.switchHandler = switchHandlerFunction;
+
+  }
+
+
+  addProject(){
+    console.log(this);
+  }
+  switchProject(prodjectId){
+    // const projectIndex = this.projects.indexOf(p => p.id === prodjectId);
+    // this.projects.splice(projectIndex, 1);
+
+    this.switchHandler(this.projects.find(p => p.id === prodjectId));
+    // removing a specific projec
+    this.projects = this.projects.filter(p => p.id !== prodjectId);
   }
 }
 
-class App{
+
+
+
+
+
+
+
+
+
+
+class App {
   static init(){
-    const activeProjectList = new ProjectList('active');
-    const finishedProjectList = new ProjectList('finished');
+    // Instatiation for Projectlist. Accessing the 2 section in HTML elements.
+    const activeProjectsList = new ProjectList('active'); 
+    const finishedProjectsList = new ProjectList('finished');
+
+    activeProjectsList.setSwitchHandlerFunction(finishedProjectsList.addProject.bind(finishedProjectsList));
+
+     finishedProjectsList.setSwitchHandlerFunction(activeProjectsList.addProject.bind(activeProjectsList));
 
   }
 }
